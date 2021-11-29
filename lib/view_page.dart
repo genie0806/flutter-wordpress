d011c0be473f57061a8d6ce27d12694d849420d0
genie0806 <genie0806@gmail.com>
2021-11-30 02:15:17 +0900

    core 디렉토리에 use_case의 basic code 생성

--- lib/view_page.dart
+++ lib/view_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:test_virtue/core/result.dart';
+import 'package:test_virtue/data/data_source/remote/simple_post_api.dart';
 import 'package:test_virtue/model/post_model/post_model.dart';
+import 'package:test_virtue/model/simple_post_model.dart';
 import 'package:wordpress_api/wordpress_api.dart';
 
 import 'package:test_virtue/data/data_source/remote/post_api.dart';
@@ -17,12 +19,14 @@ class ViewPage extends StatefulWidget {
 class _ViewPageState extends State<ViewPage> {
   final postModel = PostModel();
   final _postApi = PostApi();
+  final simplePostModel = SimplePostModel();
+  final _simplePostApi = SimplePostApi();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: FutureBuilder<List<PostModel>>(
-            future: _postApi.fetchData(),
+        body: FutureBuilder<List<SimplePostModel>>(
+            future: _simplePostApi.fetchData(),
             builder: (BuildContext context, AsyncSnapshot snapshot) {
               if (snapshot.connectionState == ConnectionState.waiting) {
                 return const Center(
@@ -39,9 +43,9 @@ class _ViewPageState extends State<ViewPage> {
                 return const Text('데어터가 없습니다.');
               }
 
-              List<PostModel> res = snapshot.data ?? [];
+              List<SimplePostModel> res = snapshot.data ?? [];
               return Column(
-                  children: res.map((e) => Text(e.title.toString())).toList());
+                  children: res.map((e) => Text(e.title ?? '없다')).toList());
             }));
   }
 }
