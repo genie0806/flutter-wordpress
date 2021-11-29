e811f87106974de3a0606c9aa2ed543e0b6e788b
genie0806 <genie0806@gmail.com>
2021-11-30 01:57:14 +0900

    simple_post_model freezed 로 만들기

--- lib/model/simple_post_model.dart
+++ lib/model/simple_post_model.dart
@@ -1,9 +1,18 @@
-//import 'package:freezed_annotation/freezed_annotation.dart';
-//import 'package:json_annotation/json_annotation.dart';
-//
-//part 'simple_post_model.freezed.dart';
-//part 'simple_post_model.g.dart';
-//
-//@freezed
-//class SimplePostModel with _$SimplePostModel {
-//  factory SimplePostModel(
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'simple_post_model.freezed.dart';
+part 'simple_post_model.g.dart';
+
+@freezed
+class SimplePostModel with _$SimplePostModel {
+  factory SimplePostModel(
+      {int? id,
+      String? postDate,
+      String? title,
+      String? categoryName,
+      String? imageUrl,
+      String? postContent}) = _SimplePostModel;
+  factory SimplePostModel.fromJson(Map<String, dynamic> json) =>
+      _$SimplePostModelFromJson(json);
+}
