ad4c410cc26e9f44c6802bb5fb76bc5e8dd2135e
genie0806 <genie0806@gmail.com>
2021-11-30 02:04:17 +0900

    simple_post_model을 받아서 사용할 simple_post_api 생성

---
+++ lib/data/data_source/remote/simple_post_api.dart
@@ -0,0 +1,17 @@
+import 'dart:convert' as convert;
+import 'package:http/http.dart' as http;
+import 'package:test_virtue/core/result.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:test_virtue/model/simple_post_model.dart';
+
+String baseUrl = dotenv.get('BASE_URL');
+
+class SimplePostApi {
+  Future<List<SimplePostModel>> fetchData() async {
+    final response = await http.get(Uri.parse(baseUrl + 'latest-posts'));
+    Iterable jsonResponse = convert.jsonDecode(response.body);
+    List<SimplePostModel> simplePostApi =
+        jsonResponse.map((e) => SimplePostModel.fromJson(e)).toList();
+    return simplePostApi;
+  }
+}
