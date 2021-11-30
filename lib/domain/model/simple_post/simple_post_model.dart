4cef161bb086e85fa9d142bc4c206d96a17edade
genie0806 <genie0806@gmail.com>
2021-12-01 05:36:13 +0900

    simple_post의 custom filed 추가로 인하여 model 수정

--- lib/domain/model/simple_post/simple_post_model.dart
+++ lib/domain/model/simple_post/simple_post_model.dart
@@ -6,13 +6,19 @@ part 'simple_post_model.g.dart';
 
 @freezed
 class SimplePostModel with _$SimplePostModel {
-  factory SimplePostModel(
-      {int? id,
-      String? postDate,
-      String? title,
-      String? categoryName,
-      String? imageUrl,
-      String? postContent}) = _SimplePostModel;
+  factory SimplePostModel({
+    int? id,
+    String? postDate,
+    String? title,
+    String? categoryName,
+    String? imageUrl,
+    String? postContent,
+    String? displayform,
+    String? space,
+    String? author,
+    String? style,
+    String? location,
+  }) = _SimplePostModel;
   factory SimplePostModel.fromJson(Map<String, dynamic> json) =>
       _$SimplePostModelFromJson(json);
 }
