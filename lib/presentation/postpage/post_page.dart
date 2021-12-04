e920ad082e32a6e3c7dbdb6db60dd9bcec462f2c
genie0806 <genie0806@gmail.com>
2021-12-05 02:49:06 +0900

    post_page의 Wrap 작성

--- lib/presentation/postpage/post_page.dart
+++ lib/presentation/postpage/post_page.dart
@@ -12,33 +12,191 @@ class PostPage extends StatefulWidget {
 class _PostPageState extends State<PostPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      //extendBodyBehindAppBar: true,
-      appBar: AppBar(
-        titleSpacing: 2,
-        //backgroundColor: Colors.transparent,
-        title: Row(
-          children: [
-            IconButton(
-                padding: EdgeInsets.zero,
-                onPressed: () {},
-                //constraints: const BoxConstraints(minWidth:),
-                icon: const Icon(
-                  Icons.arrow_back,
-                  color: Colors.white,
-                )),
-            IconButton(
-                padding: EdgeInsets.zero,
-                constraints: const BoxConstraints(minWidth: 35),
-                onPressed: () {},
-                icon: const Icon(
-                  CupertinoIcons.home,
-                  color: Colors.white,
-                )),
-          ],
-        ),
-        elevation: 0.0,
-      ),
+    return SafeArea(
+      child: Scaffold(
+          extendBodyBehindAppBar: true,
+          appBar: AppBar(
+            titleSpacing: 1,
+            toolbarHeight: 40,
+            backgroundColor: Colors.transparent,
+            title: Row(
+              children: [
+                IconButton(
+                    padding: EdgeInsets.zero,
+                    onPressed: () {},
+                    //constraints: const BoxConstraints(minWidth:),
+                    icon: const Icon(
+                      Icons.arrow_back,
+                      color: Colors.white,
+                      size: 30,
+                    )),
+                IconButton(
+                    padding: EdgeInsets.zero,
+                    constraints: const BoxConstraints(minWidth: 30),
+                    onPressed: () {},
+                    icon: const Icon(CupertinoIcons.home,
+                        color: Colors.white, size: 25)),
+              ],
+            ),
+            elevation: 0.0,
+          ),
+          body: CustomScrollView(
+            slivers: [
+              SliverAppBar(
+                pinned: true,
+                expandedHeight: 360,
+                flexibleSpace: FlexibleSpaceBar(
+                  background: Image.network(
+                    //객체로 바꿀부분
+                    'https://virtureart.shop/wp-content/uploads/2021/11/20210209%EF%BC%BF175631.jpg',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              SliverList(
+                delegate: SliverChildListDelegate(
+                  [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text('카테고리 명'),
+                        const Text(
+                          '옥적갤러리 개인전',
+                          style: TextStyle(fontSize: 40),
+                        ),
+                        Container(
+                          color: Colors.grey.shade200,
+                          width: 411,
+                          child: Wrap(
+                            direction: Axis.horizontal,
+                            //spacing: 10.0,
+                            //runSpacing: 20.0,
+                            children: [
+                              Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 15),
+                                child: Container(
+                                  width: 175.5,
+                                  height: 50,
+                                  decoration: BoxDecoration(
+                                      color: Colors.grey.shade200,
+                                      border: Border(
+                                          bottom: BorderSide(
+                                              width: 1,
+                                              color: Colors.grey.shade400))),
+                                  child: Row(children: [
+                                    Text(
+                                      '전시형태',
+                                      style: TextStyle(color: Colors.grey[700]),
+                                    ),
+                                    SizedBox(
+                                      width: 50,
+                                    ),
+                                    Text('개인전'),
+                                  ]),
+                                ),
+                              ),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 15),
+                                child: Container(
+                                  width: 175.5,
+                                  height: 50,
+                                  decoration: BoxDecoration(
+                                      color: Colors.grey.shade200,
+                                      border: Border(
+                                          bottom: BorderSide(
+                                              width: 1,
+                                              color: Colors.grey.shade400))),
+                                  child: Row(children: [
+                                    Text(
+                                      '전시공간',
+                                      style: TextStyle(color: Colors.grey[700]),
+                                    ),
+                                    SizedBox(
+                                      width: 50,
+                                    ),
+                                    Text('갤러리'),
+                                  ]),
+                                ),
+                              ),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 15),
+                                child: Container(
+                                  width: 175.5,
+                                  height: 50,
+                                  decoration: BoxDecoration(
+                                      color: Colors.grey.shade200,
+                                      border: Border(
+                                          bottom: BorderSide(
+                                              width: 1,
+                                              color: Colors.grey.shade400))),
+                                  child: Row(children: [
+                                    Text(
+                                      '작품 스타일',
+                                      style: TextStyle(color: Colors.grey[700]),
+                                    ),
+                                    SizedBox(
+                                      width: 35,
+                                    ),
+                                    Text('모던'),
+                                  ]),
+                                ),
+                              ),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 15),
+                                child: Container(
+                                  width: 175.5,
+                                  height: 50,
+                                  decoration: BoxDecoration(
+                                      color: Colors.grey.shade200,
+                                      border: Border(
+                                          bottom: BorderSide(
+                                              width: 1,
+                                              color: Colors.grey.shade400))),
+                                  child: Row(children: [
+                                    Text(
+                                      '작가',
+                                      style: TextStyle(color: Colors.grey[700]),
+                                    ),
+                                    SizedBox(
+                                      width: 76,
+                                    ),
+                                    Text('양해웅 작가'),
+                                  ]),
+                                ),
+                              ),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 15),
+                                child: Container(
+                                  width: 351,
+                                  height: 50,
+                                  color: Colors.grey.shade200,
+                                  child: Row(children: [
+                                    Text(
+                                      '지역',
+                                      style: TextStyle(color: Colors.grey[700]),
+                                    ),
+                                    SizedBox(
+                                      width: 76,
+                                    ),
+                                    Text('전라남도 여수'),
+                                  ]),
+                                ),
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ],
+          )),
     );
   }
 }
