a303584d54b51ea9b36aa40e688c068d157218f9
genie0806 <genie0806@gmail.com>
2021-12-09 02:02:40 +0900

    PostListPage PostPage 맵핑시 Provider  사용, Grayboard 수정

--- lib/presentation/postpage/components/post_board.dart
+++ lib/presentation/postpage/components/post_board.dart
@@ -19,71 +19,82 @@ class PostBoard extends StatelessWidget {
         //spacing: 10.0,
         //runSpacing: 20.0,
         children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 15),
-            child: Container(
-              width: 175.5,
-              height: 50,
-              decoration: BoxDecoration(
-                  color: Colors.grey.shade200,
-                  border: Border(
-                      bottom:
-                          BorderSide(width: 1, color: Colors.grey.shade400))),
-              child: Row(children: [
-                Text(
-                  '작가',
-                  style: TextStyle(color: Colors.grey[700]),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Flexible(
+                flex: 1,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15),
+                  child: Container(
+                    //width: 175.5,
+                    height: 50,
+                    decoration: BoxDecoration(
+                        color: Colors.grey.shade200,
+                        border: Border(
+                            bottom: BorderSide(
+                                width: 1, color: Colors.grey.shade400))),
+                    child: Row(children: [
+                      Text(
+                        '작가',
+                        style: TextStyle(color: Colors.grey[700]),
+                      ),
+                      const SizedBox(),
+                      Text(widget.model.author ?? ''),
+                    ]),
+                  ),
                 ),
-                const SizedBox(
-                  width: 76,
+              ),
+              Flexible(
+                flex: 1,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15),
+                  child: Container(
+                    //width: 175.5,
+                    height: 50,
+                    decoration: BoxDecoration(
+                        color: Colors.grey.shade200,
+                        border: Border(
+                            bottom: BorderSide(
+                                width: 1, color: Colors.grey.shade400))),
+                    child: Row(children: [
+                      Text(
+                        '전시형태',
+                        style: TextStyle(color: Colors.grey[700]),
+                      ),
+                      const SizedBox(),
+                      Text(widget.model.displayform ?? ''),
+                    ]),
+                  ),
                 ),
-                Text(widget.model.author ?? ''),
-              ]),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 15),
-            child: Container(
-              width: 175.5,
-              height: 50,
-              decoration: BoxDecoration(
-                  color: Colors.grey.shade200,
-                  border: Border(
-                      bottom:
-                          BorderSide(width: 1, color: Colors.grey.shade400))),
-              child: Row(children: [
-                Text(
-                  '전시형태',
-                  style: TextStyle(color: Colors.grey[700]),
-                ),
-                const SizedBox(
-                  width: 50,
-                ),
-                Text(widget.model.displayform ?? ''),
-              ]),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 15),
-            child: Container(
-              width: 175.5,
-              height: 50,
-              decoration: BoxDecoration(
-                  color: Colors.grey.shade200,
-                  border: Border(
-                      bottom:
-                          BorderSide(width: 1, color: Colors.grey.shade400))),
-              child: Row(children: [
-                Text(
-                  '작품 스타일',
-                  style: TextStyle(color: Colors.grey[700]),
-                ),
-                const SizedBox(
-                  width: 35,
-                ),
-                Text(widget.model.style ?? ''),
-              ]),
-            ),
+              ),
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 15),
+                    child: Container(
+                      width: 175.5,
+                      height: 50,
+                      decoration: BoxDecoration(
+                          color: Colors.grey.shade200,
+                          border: Border(
+                              bottom: BorderSide(
+                                  width: 1, color: Colors.grey.shade400))),
+                      child: Row(children: [
+                        Text(
+                          '작품 스타일',
+                          style: TextStyle(color: Colors.grey[700]),
+                        ),
+                        const SizedBox(
+                          width: 35,
+                        ),
+                        Text(widget.model.style ?? ''),
+                      ]),
+                    ),
+                  ),
+                ],
+              ),
+            ],
           ),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 15),
--- lib/presentation/postpage/post_page.dart
+++ lib/presentation/postpage/post_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter_wordpress_content/wp_content.dart';
 import 'package:provider/provider.dart';
 import 'package:test_virtue/core/data_case.dart';
 import 'package:test_virtue/domain/model/simple_post_model/simple_post_model.dart';
+import 'package:test_virtue/presentation/postpage/components/board.dart';
 import 'package:test_virtue/presentation/postpage/components/follow_button.dart';
 import 'package:test_virtue/presentation/postpage/components/post_board.dart';
 import 'package:test_virtue/presentation/postpage/post_page_event.dart';
@@ -50,145 +51,160 @@ class _PostPageState extends State<PostPage> {
 
     return SafeArea(
       child: Scaffold(
-          extendBodyBehindAppBar: true,
-          appBar: AppBar(
-            titleSpacing: 1,
-            toolbarHeight: 40,
-            backgroundColor: Colors.transparent,
-            automaticallyImplyLeading: false,
-            shadowColor: const Color(0xffFEFEFE),
-            elevation: 0.2,
-            title: Row(
-              children: [
-                IconButton(
-                    padding: EdgeInsets.zero,
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    //constraints: const BoxConstraints(minWidth:),
-                    icon: const Icon(
-                      Icons.arrow_back,
-                      color: Colors.white,
-                      size: 30,
-                    )),
-                IconButton(
-                    padding: EdgeInsets.zero,
-                    constraints: const BoxConstraints(minWidth: 35),
-                    onPressed: () {},
-                    icon: const Icon(CupertinoIcons.home,
-                        color: Colors.white, size: 25)),
-              ],
-            ),
+        extendBodyBehindAppBar: true,
+        appBar: AppBar(
+          titleSpacing: 1,
+          toolbarHeight: 40,
+          backgroundColor: Colors.transparent,
+          automaticallyImplyLeading: false,
+          shadowColor: const Color(0xffFEFEFE),
+          elevation: 0.2,
+          title: Row(
+            children: [
+              IconButton(
+                  padding: EdgeInsets.zero,
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  //constraints: const BoxConstraints(minWidth:),
+                  icon: const Icon(
+                    Icons.arrow_back,
+                    color: Colors.white,
+                    size: 30,
+                  )),
+              IconButton(
+                  padding: EdgeInsets.zero,
+                  constraints: const BoxConstraints(minWidth: 35),
+                  onPressed: () {},
+                  icon: const Icon(CupertinoIcons.home,
+                      color: Colors.white, size: 25)),
+            ],
           ),
-          body: CustomScrollView(
-            slivers: [
-              SliverAppBar(
-                toolbarHeight: 0,
-                automaticallyImplyLeading: false,
-                backgroundColor: Colors.transparent,
-                pinned: true,
-                floating: false,
-                snap: false,
-                expandedHeight: 370,
-                stretch: true,
-                flexibleSpace: FlexibleSpaceBar(
-                  background: Image.network(
-                    widget.model.largeUrl ?? '',
-                    fit: BoxFit.cover,
-                  ),
+        ),
+        body: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              toolbarHeight: 0,
+              automaticallyImplyLeading: false,
+              backgroundColor: Colors.transparent,
+              pinned: true,
+              floating: false,
+              snap: false,
+              expandedHeight: 370,
+              stretch: true,
+              flexibleSpace: FlexibleSpaceBar(
+                background: Image.network(
+                  widget.model.largeUrl ?? '',
+                  fit: BoxFit.cover,
                 ),
               ),
-              SliverList(
-                delegate: SliverChildListDelegate(
-                  [
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(17, 25, 17, 15),
-                      child: Text(
-                        widget.model.categoryName ?? '',
-                        style: const TextStyle(
-                            fontSize: 15, fontWeight: FontWeight.bold),
-                      ),
+            ),
+            SliverList(
+              delegate: SliverChildListDelegate(
+                [
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(17, 25, 17, 15),
+                    child: Text(
+                      widget.model.categoryName ?? '',
+                      style: const TextStyle(
+                          fontSize: 15, fontWeight: FontWeight.bold),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(17, 0, 17, 20),
-                      child: Text(
-                        widget.model.title ?? '',
-                        style: const TextStyle(
-                            fontSize: 25, fontWeight: FontWeight.bold),
-                      ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(17, 0, 17, 20),
+                    child: Text(
+                      widget.model.title ?? '',
+                      style: const TextStyle(
+                          fontSize: 25, fontWeight: FontWeight.bold),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(17, 0, 17, 30),
-                      child: Row(
-                        children: [
-                          CircleAvatar(
-                            radius: 22,
-                            child: ClipOval(
-                              child: Image.network(
-                                widget.model.profileUrl ?? '',
-                                fit: BoxFit.scaleDown,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(17, 0, 17, 30),
+                    child: Row(
+                      children: [
+                        CircleAvatar(
+                          radius: 22,
+                          child: ClipOval(
+                            child: Image.network(
+                              widget.model.profileUrl ?? '',
+                              fit: BoxFit.scaleDown,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(12, 0, 0, 0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                widget.model.profile ?? '',
+                                style: const TextStyle(fontSize: 14),
                               ),
-                            ),
+                              const SizedBox(
+                                height: 4,
+                              ),
+                              Text(
+                                dateform,
+                                style: const TextStyle(
+                                    fontSize: 14, color: Colors.grey),
+                              ),
+                            ],
                           ),
-                          Padding(
-                            padding: const EdgeInsets.fromLTRB(12, 0, 0, 0),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  widget.model.profile ?? '',
-                                  style: const TextStyle(fontSize: 14),
-                                ),
-                                const SizedBox(
-                                  height: 4,
-                                ),
-                                Text(
-                                  dateform,
-                                  style: const TextStyle(
-                                      fontSize: 14, color: Colors.grey),
-                                ),
-                              ],
-                            ),
-                          ),
-                          const SizedBox(
-                            width: 135,
-                          ),
-                          ElevatedButton(
-                              onPressed: () {
+                        ),
+                        const Expanded(
+                          child: SizedBox(),
+                        ),
+                        ElevatedButton(
+                            onPressed: () {
+                              setState(() {
                                 pressed = !pressed;
-                              },
-                              style: pressed
-                                  ? ElevatedButton.styleFrom(
-                                      primary: Colors.black)
-                                  : ElevatedButton.styleFrom(
-                                      primary: Colors.white),
-                              child: pressed
-                                  ? const Text('Follow')
-                                  : const Text(
-                                      'Following',
-                                      style: TextStyle(color: Colors.black),
-                                    )),
-                        ],
-                      ),
+                              });
+                            },
+                            style: pressed
+                                ? ElevatedButton.styleFrom(
+                                    primary: Colors.black,
+                                    maximumSize: const Size(93, 40),
+                                    minimumSize: const Size(93, 40),
+                                  )
+                                : ElevatedButton.styleFrom(
+                                    primary: Colors.white,
+                                    maximumSize: const Size(93, 40),
+                                    minimumSize: const Size(93, 40),
+                                    side: const BorderSide(color: Colors.black),
+                                  ),
+                            child: pressed
+                                ? const Text('Follow')
+                                : const Text(
+                                    'Following',
+                                    style: TextStyle(color: Colors.black),
+                                  )),
+                      ],
                     ),
-                    //회색 포스트 보드
-                    PostBoard(widget: widget),
-                    WPContent(
+                  ),
+                  //회색 포스트 보드
+                  Greyboard(
+                    widget: widget,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 15),
+                    child: WPContent(
                       widget.model.postContent.toString(),
                       headingTextColor: Colors.black,
                       paragraphTextColor: Colors.black,
                       imageCaptionTextColor: Colors.black,
                       //textDirection: TextDirection.ltr,
                       fontFamily: 'my_font_family',
-                      fontSize: 16.0,
+                      fontSize: 17.0,
                       paragraphArabicIdentifier: 'tk-adobe-arabic',
                       arabicFontFamily: 'my_arabic_font_family',
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-            ],
-          )),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
