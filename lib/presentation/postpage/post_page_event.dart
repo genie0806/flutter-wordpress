f070ae077380e4c12789bb56c5ac12e543e7d78d
genie0806 <genie0806@gmail.com>
2021-12-09 05:30:01 +0900

    PostPageEvent  freezed로 변환

--- lib/presentation/postpage/post_page_event.dart
+++ lib/presentation/postpage/post_page_event.dart
@@ -1,10 +1,9 @@
-abstract class PostPageEvent {
-  factory PostPageEvent.showSnackBar(String message) = Showsnackbar;
-}
+import 'package:freezed_annotation/freezed_annotation.dart';
 
-class Showsnackbar implements PostPageEvent {
-  final String message;
-  Showsnackbar(
-    this.message,
-  );
+part 'post_page_event.freezed.dart';
+
+@freezed
+abstract class PostPageEvent {
+  const factory PostPageEvent.showSnackBar(String message) = Showsnackbar;
+  const factory PostPageEvent.reloadPage() = ReloadPage;
 }
