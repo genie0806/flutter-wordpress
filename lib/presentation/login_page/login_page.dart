fc8f3f3fe04d410be6d8ad102e9802738111c7c3
genie0806 <genie0806@gmail.com>
2021-12-16 01:43:58 +0900

    Logout 기능 수정완료

--- lib/presentation/login_page/login_page.dart
+++ lib/presentation/login_page/login_page.dart
@@ -19,38 +19,103 @@ class _LoginPageState extends State<LoginPage> {
       appBar: AppBar(
         title: Text('test'),
       ),
-      body: Center(
-          child: model != null
-              ? Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    TextButton(
-                        onPressed: () {
-                          viewModel.googleLogin();
-                        },
-                        child: Text('구글 로그인'))
-                  ],
-                )
-              : Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    CircleAvatar(
-                      backgroundImage: Image.network(viewModel.useCases
-                                  .getSocialLogin.googleAccount?.photoUrl ??
-                              "")
-                          .image,
-                      radius: 50,
-                    ),
-                    Text(model.displayName ?? ''),
-                    Text(model.email ?? ''),
-                    ActionChip(
-                        label: Text("LogOut"),
-                        onPressed: () {
-                          viewModel.googleLogout();
-                        })
-                  ],
-                )),
+      body: LoginUi(),
+      //     Center(
+      //         child: model != null
+      //             ? Column(
+      //                 mainAxisAlignment: MainAxisAlignment.center,
+      //                 children: [
+      //                   TextButton(
+      //                       onPressed: () {
+      //                         viewModel.googleLogin();
+      //                       },
+      //                       child: Text('구글 로그인'))
+      //                 ],
+      //               )
+      //             : Column(
+      //                 mainAxisAlignment: MainAxisAlignment.center,
+      //                 crossAxisAlignment: CrossAxisAlignment.center,
+      //                 children: [
+      //                   CircleAvatar(
+      //                     backgroundImage: Image.network(viewModel.useCases
+      //                                 .getSocialLogin.googleAccount?.photoUrl ??
+      //                             "")
+      //                         .image,
+      //                     radius: 50,
+      //                   ),
+      //                   Text(model.displayName ?? ''),
+      //                   Text(model.email ?? ''),
+      //                   ActionChip(
+      //                       label: Text("LogOut"),
+      //                       onPressed: () {
+      //                         viewModel.googleLogout();
+      //                       })
+      //                 ],
+      //               )),
+    );
+  }
+}
+
+class LoginUi extends StatelessWidget {
+  const LoginUi({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final viewModel = context.watch<LoginViewModel>();
+    final model = SocialUserModel();
+    if (viewModel.useCases.getSocialLogin.googleAccount != null) {
+      return Center(child: LoggedUi());
+    } else {
+      return LoginControls();
+    }
+  }
+}
+
+class LoggedUi extends StatelessWidget {
+  const LoggedUi({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final viewModel = context.watch<LoginViewModel>();
+    final model = SocialUserModel();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        CircleAvatar(
+          backgroundImage: NetworkImage(
+              viewModel.useCases.getSocialLogin.googleAccount?.photoUrl ?? ""),
+          radius: 50,
+        ),
+        Text(viewModel.useCases.getSocialLogin.socialUserModel?.displayName ??
+            ''),
+        Text(viewModel.useCases.getSocialLogin.socialUserModel?.email ?? ''),
+        ActionChip(
+            label: Text("LogOut"),
+            onPressed: () {
+              viewModel.googleLogout();
+            })
+      ],
+    );
+  }
+}
+
+class LoginControls extends StatelessWidget {
+  const LoginControls({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final viewModel = context.watch<LoginViewModel>();
+    final model = SocialUserModel();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        TextButton(
+            onPressed: () {
+              viewModel.googleLogin();
+            },
+            child: Text('구글 로그인'))
+      ],
     );
   }
 }
