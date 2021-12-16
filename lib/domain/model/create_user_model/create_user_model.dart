afa161f20f2b1bbeea5d9360925086fb25e836ee
genie0806 <genie0806@gmail.com>
2021-12-17 03:21:31 +0900

    create User 모델의 lastname 삭제 , nickname 추가

--- lib/domain/model/create_user_model/create_user_model.dart
+++ lib/domain/model/create_user_model/create_user_model.dart
@@ -6,11 +6,19 @@ part 'create_user_model.g.dart';
 
 @freezed
 class CreateUserModel with _$CreateUserModel {
-  factory CreateUserModel(
-      {String? password,
-      String? email,
-      String? lastName,
-      String? firstName}) = _CreateUserModel;
+  factory CreateUserModel({
+    String? password,
+    String? email,
+    String? firstName,
+    String? nickname,
+  }) = _CreateUserModel;
   factory CreateUserModel.fromJson(Map<String, dynamic> json) =>
       _$CreateUserModelFromJson(json);
 }
+
+
+//'password': instance.password,
+//      'email': instance.email,
+//      'nickname': instance.nickname,
+//      'first_name': instance.firstName,
+//      'username': instance.email,
