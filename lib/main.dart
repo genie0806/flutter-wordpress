5a5ac343ac4d9622dd82d5d4617d052dca97b5ee
genie0806 <genie0806@gmail.com>
2021-12-21 00:28:14 +0900

    Sign Up Page  리펙토링시작

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:virtue_test/di/provider_setup.dart';
 import 'package:virtue_test/presentation/login_page/login_page.dart';
 
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:virtue_test/presentation/login_page/sign_up_page.dart';
+import 'package:virtue_test/presentation/create_user_page/create_user_page.dart';
 
 Future main() async {
   await dotenv.load(fileName: 'url.env');
