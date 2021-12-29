cf25c41ce9d1d82d23347fe5cf801ffb173203ca
genie0806 <genie0806@gmail.com>
2021-12-29 14:39:31 +0900

    Post Page btm 추가

--- lib/presentation/post_page/post_page.dart
+++ lib/presentation/post_page/post_page.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:ionicons/ionicons.dart';
+import 'package:line_icons/line_icons.dart';
 import 'package:provider/provider.dart';
 import 'package:virtue_test/presentation/post_list_page/post_list_page_view_model.dart';
 import 'package:virtue_test/presentation/post_page/components/content_board.dart';
@@ -23,6 +25,8 @@ class PostPage extends StatefulWidget {
 class _PostPageState extends State<PostPage> {
   StreamSubscription? streamSubscription;
   bool pressed = true;
+  bool likesPressed = true;
+  bool commentPressed = true;
 
   @override
   void initState() {
@@ -60,8 +64,8 @@ class _PostPageState extends State<PostPage> {
           toolbarHeight: 40,
           backgroundColor: Colors.transparent,
           automaticallyImplyLeading: false,
-          shadowColor: const Color(0xffFEFEFE),
-          elevation: 0.2,
+          shadowColor: const Color(0xffFEFEFE).withAlpha(1),
+          //elevation: 0.1,
           title: Row(
             children: [
               IconButton(
@@ -88,40 +92,111 @@ class _PostPageState extends State<PostPage> {
         ),
         body: RefreshIndicator(
           onRefresh: viewModel.refreshList,
-          child: CustomScrollView(
-            slivers: [
-              SliverImage(simplePostModel: model),
-              SliverList(
-                delegate: SliverChildListDelegate(
-                  [
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(17, 25, 17, 15),
-                      child: Text(
-                        model.categoryName ?? '',
-                        style: const TextStyle(
-                            fontSize: 15, fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(17, 0, 17, 20),
-                      child: Text(
-                        model.title ?? '',
-                        style: const TextStyle(
-                            fontSize: 25, fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                    ProfileBoard(
-                      simplePostModel: model,
-                    ),
+          child: Stack(
+            children: [
+              CustomScrollView(
+                slivers: [
+                  SliverImage(simplePostModel: model),
+                  SliverList(
+                    delegate: SliverChildListDelegate(
+                      [
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(17, 25, 17, 15),
+                          child: Text(
+                            model.categoryName ?? '',
+                            style: const TextStyle(
+                                fontSize: 15, fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(17, 0, 17, 20),
+                          child: Text(
+                            model.title ?? '',
+                            style: const TextStyle(
+                                fontSize: 25, fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        ProfileBoard(
+                          simplePostModel: model,
+                        ),
 
-                    //회색 포스트 보드
-                    Infoboard(
-                      simplePostModel: model,
+                        //회색 포스트 보드
+                        Infoboard(
+                          simplePostModel: model,
+                        ),
+                        ContentBoard(
+                          simplePostModel: model,
+                        ),
+                      ],
                     ),
-                    ContentBoard(
-                      simplePostModel: model,
+                  ),
+                ],
+              ),
+              Positioned(
+                left: 0,
+                top: 790,
+                right: 0,
+                bottom: 0,
+                child: Opacity(
+                  opacity: 0.9,
+                  child: Container(
+                    color: Colors.white,
+                    height: 55,
+                    child: Row(
+                      //crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        ElevatedButton.icon(
+                            style: ElevatedButton.styleFrom(
+                                primary: Colors.white, elevation: 0),
+                            onPressed: () {
+                              setState(() {
+                                likesPressed = !likesPressed;
+                              });
+                            },
+                            icon: likesPressed
+                                ? const Icon(
+                                    Icons.favorite_border_outlined,
+                                    color: Colors.black,
+                                  )
+                                : const Icon(
+                                    Icons.favorite,
+                                    color: Color(0xff405376),
+                                  ),
+                            label: likesPressed
+                                ? const Text(
+                                    '0',
+                                    style: TextStyle(color: Colors.black),
+                                  )
+                                : const Text(
+                                    '1',
+                                    style: TextStyle(color: Colors.black),
+                                  )),
+                        IconButton(
+                            onPressed: () {},
+                            icon: const Icon(Ionicons.share_social_outline)),
+                        ElevatedButton.icon(
+                            style: ElevatedButton.styleFrom(
+                                primary: Colors.white, elevation: 0),
+                            onPressed: () {
+                              setState(() {
+                                commentPressed = !commentPressed;
+                              });
+                              //navigator로 Comment 창으로 이동
+                            },
+                            icon: const Icon(
+                              Ionicons.chatbubble_outline,
+                              color: Colors.black,
+                            ),
+                            label: commentPressed
+                                ? const Text(
+                                    '0',
+                                    style: TextStyle(color: Colors.black),
+                                  )
+                                : const Text('1',
+                                    style: TextStyle(color: Colors.black))),
+                      ],
                     ),
-                  ],
+                  ),
                 ),
               ),
             ],
