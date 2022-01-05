3163518824a5e673536f12c429007399b77ccbf5
genie0806 <genie0806@gmail.com>
2022-01-06 03:05:57 +0900

    UserMe repository, impl 작성

---
+++ lib/data/respository/user_me_repostiory_impl.dart
@@ -0,0 +1,17 @@
+import 'package:virtue_test/core/result.dart';
+import 'package:virtue_test/data/data_source/remote/user_me_api.dart';
+import 'package:virtue_test/domain/model/user_me_model/user_me_model.dart';
+import 'package:virtue_test/domain/repository/user_me_repository.dart';
+
+class UserMeRepositoryImpl implements UserMeRepository {
+  UserMeApi userMeApi;
+
+  UserMeRepositoryImpl(
+    this.userMeApi,
+  );
+
+  @override
+  Future<Result<UserMeModel>> getUserMe() async {
+    return await userMeApi.fetchUserMe();
+  }
+}
