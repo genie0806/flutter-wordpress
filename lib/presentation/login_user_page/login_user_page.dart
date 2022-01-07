ee3a5e8fc4032a12f251a98deb9887d466969c2e
genie0806 <genie0806@gmail.com>
2022-01-08 04:12:59 +0900

    safearea에 색상추가(흰색이 정확히는 안된다)

--- lib/presentation/login_user_page/login_user_page.dart
+++ lib/presentation/login_user_page/login_user_page.dart
@@ -8,6 +8,7 @@ import 'package:virtue_test/presentation/login_user_page/components/text_field_i
 import 'package:virtue_test/presentation/login_user_page/login_user_event.dart';
 import 'package:virtue_test/presentation/login_user_page/login_user_view_model.dart';
 import 'package:virtue_test/presentation/post_list_page/post_list_page.dart';
+import 'package:colorful_safe_area/colorful_safe_area.dart';
 
 class LoginUserPage extends StatefulWidget {
   const LoginUserPage({Key? key}) : super(key: key);
@@ -74,135 +75,132 @@ class _LoginUserPageState extends State<LoginUserPage> {
 
     final viewModel = context.watch<LoginUserViewModel>();
     final state = viewModel.state;
-    return Container(
+    return ColorfulSafeArea(
       color: Colors.white,
-      child: SafeArea(
-        child: Scaffold(
-          backgroundColor: Colors.white,
-          appBar: AppBar(
-              titleSpacing: 6,
-              automaticallyImplyLeading: false,
-              foregroundColor: Colors.black,
-              backgroundColor: Colors.white,
-              elevation: 0,
-              title: Row(
-                children: [
-                  IconButton(
-                    padding: const EdgeInsets.only(top: 5),
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    icon: Image.asset(
-                      'assets/noun-arrow-left-1476218.png',
-                      width: 26,
-                      height: 26,
-                      color: Colors.black,
-                    ),
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+            titleSpacing: 6,
+            automaticallyImplyLeading: false,
+            foregroundColor: Colors.black,
+            backgroundColor: Colors.white,
+            elevation: 0,
+            title: Row(
+              children: [
+                IconButton(
+                  padding: const EdgeInsets.only(top: 5),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  icon: Image.asset(
+                    'assets/noun-arrow-left-1476218.png',
+                    width: 26,
+                    height: 26,
+                    color: Colors.black,
                   ),
-                  const Padding(
-                    padding: EdgeInsets.only(left: 100),
-                    child: Text(
-                      '이메일 로그인',
-                      style:
-                          TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
-                      textAlign: TextAlign.start,
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(left: 100),
+                  child: Text(
+                    '이메일 로그인',
+                    style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.start,
+                  ),
+                ),
+              ],
+            )),
+        body: SingleChildScrollView(
+          child: Form(
+            key: globalFormKey,
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(20, 10, 20, 0),
+              child: Column(
+                children: [
+                  Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 15),
+                      child: TextFormField(
+                        initialValue: "",
+                        autovalidateMode: AutovalidateMode.onUserInteraction,
+                        validator: (onValidateVal) {
+                          if (onValidateVal!.trim().isEmpty) {
+                            return '이메일을 입력해주세요';
+                          }
+                          if (!onValidateVal.contains('@')) {
+                            return '이메일 형식으로 입력해주세요';
+                          }
+                          if (!onValidateVal.contains('.')) {
+                            return '이메일 형식으로 입력해주세요';
+                          }
+                          return null;
+                        },
+                        onChanged: (val) {
+                          viewModel.onEvent(StoreUsername(val));
+                        },
+                        onSaved: (value) {
+                          viewModel
+                              .onEvent(StoreUsername(value.toString().trim()));
+                        },
+                        cursorColor: const Color(0xff405376),
+                        style: const TextStyle(
+                          fontSize: 16,
+                          color: Colors.black,
+                        ),
+                        decoration: textInputDeco('이메일'),
+                      )),
+                  Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 15),
+                      child: TextFormField(
+                        initialValue: "",
+                        autovalidateMode: AutovalidateMode.onUserInteraction,
+                        obscureText: state.hidePassword,
+                        validator: (onValidateVal) {
+                          if (onValidateVal!.trim().isEmpty) {
+                            return '비밀번호를 입력해주세요';
+                          }
+                          if (onValidateVal.length < 8) {
+                            return '비밀번호는 영문 숫자를 포함하여 8자 이상이어야 합니다.';
+                          }
+                          return null;
+                        },
+                        onChanged: (val) {
+                          viewModel.onEvent(StorePassword(val));
+                        },
+                        onSaved: (value) {
+                          viewModel
+                              .onEvent(StorePassword(value.toString().trim()));
+                        },
+                        cursorColor: const Color(0xff405376),
+                        style: const TextStyle(
+                          fontSize: 16,
+                          color: Colors.black,
+                        ),
+                        decoration: passwordInputDeco(viewModel, state),
+                      )),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 20),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        FocusScope.of(context).requestFocus(FocusNode());
+
+                        if (validateAndSave()) {
+                          viewModel.onEvent(LoginUserEvent.loginUser(
+                              state.username, state.password));
+                        }
+                      },
+                      child: const Text(
+                        '로그인하기',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 15),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        primary: const Color(0xff405479),
+                        //maximumSize: const Size(375, 50),
+                        minimumSize: const Size(375, 55),
+                        //side: const BorderSide(color: Colors.black, width: 0.5),
+                      ),
                     ),
                   ),
                 ],
-              )),
-          body: SingleChildScrollView(
-            child: Form(
-              key: globalFormKey,
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(20, 10, 20, 0),
-                child: Column(
-                  children: [
-                    Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 15),
-                        child: TextFormField(
-                          initialValue: "",
-                          autovalidateMode: AutovalidateMode.onUserInteraction,
-                          validator: (onValidateVal) {
-                            if (onValidateVal!.trim().isEmpty) {
-                              return '이메일을 입력해주세요';
-                            }
-                            if (!onValidateVal.contains('@')) {
-                              return '이메일 형식으로 입력해주세요';
-                            }
-                            if (!onValidateVal.contains('.')) {
-                              return '이메일 형식으로 입력해주세요';
-                            }
-                            return null;
-                          },
-                          onChanged: (val) {
-                            viewModel.onEvent(StoreUsername(val));
-                          },
-                          onSaved: (value) {
-                            viewModel.onEvent(
-                                StoreUsername(value.toString().trim()));
-                          },
-                          cursorColor: const Color(0xff405376),
-                          style: const TextStyle(
-                            fontSize: 16,
-                            color: Colors.black,
-                          ),
-                          decoration: textInputDeco('이메일'),
-                        )),
-                    Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 15),
-                        child: TextFormField(
-                          initialValue: "",
-                          autovalidateMode: AutovalidateMode.onUserInteraction,
-                          obscureText: state.hidePassword,
-                          validator: (onValidateVal) {
-                            if (onValidateVal!.trim().isEmpty) {
-                              return '비밀번호를 입력해주세요';
-                            }
-                            if (onValidateVal.length < 8) {
-                              return '비밀번호는 영문 숫자를 포함하여 8자 이상이어야 합니다.';
-                            }
-                            return null;
-                          },
-                          onChanged: (val) {
-                            viewModel.onEvent(StorePassword(val));
-                          },
-                          onSaved: (value) {
-                            viewModel.onEvent(
-                                StorePassword(value.toString().trim()));
-                          },
-                          cursorColor: const Color(0xff405376),
-                          style: const TextStyle(
-                            fontSize: 16,
-                            color: Colors.black,
-                          ),
-                          decoration: passwordInputDeco(viewModel, state),
-                        )),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 20),
-                      child: ElevatedButton(
-                        onPressed: () {
-                          FocusScope.of(context).requestFocus(FocusNode());
-
-                          if (validateAndSave()) {
-                            viewModel.onEvent(LoginUserEvent.loginUser(
-                                state.username, state.password));
-                          }
-                        },
-                        child: const Text(
-                          '로그인하기',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold, fontSize: 15),
-                        ),
-                        style: ElevatedButton.styleFrom(
-                          primary: const Color(0xff405479),
-                          //maximumSize: const Size(375, 50),
-                          minimumSize: const Size(375, 55),
-                          //side: const BorderSide(color: Colors.black, width: 0.5),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
               ),
             ),
           ),
--- lib/presentation/post_list_page/post_list_page.dart
+++ lib/presentation/post_list_page/post_list_page.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'package:colorful_safe_area/colorful_safe_area.dart';
 import 'package:flutter/material.dart';
 import 'package:ionicons/ionicons.dart';
 import 'package:line_icons/line_icons.dart';
@@ -54,167 +55,172 @@ class _PostListPageState extends State<PostListPage> {
   @override
   Widget build(BuildContext context) {
     final viewModel = context.watch<PostListPageViewModel>();
-    return SafeArea(
+    return ColorfulSafeArea(
+        color: Colors.white,
         child: Scaffold(
-      backgroundColor: Colors.white,
-      body: DefaultTabController(
-        length: 4,
-        child: NestedScrollView(
-          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-            return [
-              SliverAppBar(
-                automaticallyImplyLeading: false,
-                pinned: false,
-                floating: false,
-                snap: false,
-                toolbarHeight: 45,
-                backgroundColor: Colors.white,
-                flexibleSpace: FlexibleSpaceBar(
-                  background: Padding(
-                    padding: const EdgeInsets.fromLTRB(15, 0, 10, 0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Expanded(
-                          child: SizedBox(
-                            height: 34,
-                            child: TextField(
-                              decoration: InputDecoration(
-                                border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(5),
-                                  borderSide: const BorderSide(
-                                    width: 0,
-                                    style: BorderStyle.none,
+          backgroundColor: Colors.white,
+          body: DefaultTabController(
+            length: 4,
+            child: NestedScrollView(
+              headerSliverBuilder:
+                  (BuildContext context, bool innerBoxIsScrolled) {
+                return [
+                  SliverAppBar(
+                    automaticallyImplyLeading: false,
+                    pinned: false,
+                    floating: false,
+                    snap: false,
+                    toolbarHeight: 45,
+                    backgroundColor: Colors.white,
+                    flexibleSpace: FlexibleSpaceBar(
+                      background: Padding(
+                        padding: const EdgeInsets.fromLTRB(15, 0, 10, 0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Expanded(
+                              child: SizedBox(
+                                height: 34,
+                                child: TextField(
+                                  decoration: InputDecoration(
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(5),
+                                      borderSide: const BorderSide(
+                                        width: 0,
+                                        style: BorderStyle.none,
+                                      ),
+                                    ),
+                                    contentPadding:
+                                        const EdgeInsets.only(top: 5),
+                                    fillColor: const Color(0xffF3F3F3),
+                                    filled: true,
+                                    prefixIcon: Icon(
+                                      Ionicons.search_outline,
+                                      color: Colors.grey.shade500,
+                                    ),
+                                    hintText: 'Virtue 검색',
+                                    hintStyle: TextStyle(
+                                        fontSize: 16,
+                                        color: Colors.grey.shade400),
                                   ),
                                 ),
-                                contentPadding: const EdgeInsets.only(top: 5),
-                                fillColor: const Color(0xffF3F3F3),
-                                filled: true,
-                                prefixIcon: Icon(
-                                  Ionicons.search_outline,
-                                  color: Colors.grey.shade500,
-                                ),
-                                hintText: 'Virtue 검색',
-                                hintStyle: TextStyle(
-                                    fontSize: 16, color: Colors.grey.shade400),
                               ),
                             ),
-                          ),
-                        ),
-                        const SizedBox(
-                          width: 9,
-                        ),
-                        Icon(
-                          LineIcons.bookmarkAlt,
-                          size: 34,
-                          color: Colors.grey.shade600,
-                        ),
-                        const SizedBox(
-                          width: 4,
-                        ),
-                        Icon(
-                          LineIcons.shoppingCart,
-                          size: 39,
-                          color: Colors.grey.shade600,
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-              SliverAppBar(
-                automaticallyImplyLeading: false,
-                pinned: true,
-                floating: false,
-                //expandedHeight: 0,
-                toolbarHeight: 45,
-                backgroundColor: Colors.white,
-                flexibleSpace: FlexibleSpaceBar(
-                    background: Padding(
-                  padding: const EdgeInsets.only(left: 2),
-                  child: Container(
-                    decoration: const BoxDecoration(
-                        border: Border(
-                            bottom:
-                                BorderSide(color: Colors.grey, width: 0.3))),
-                    child: const TabBar(
-                      isScrollable: true,
-                      unselectedLabelColor: Colors.black,
-                      labelColor: Color(0xff405376),
-                      labelStyle:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
-                      indicator: UnderlineTabIndicator(
-                          borderSide:
-                              BorderSide(width: 2, color: Color(0xff405376)),
-                          insets: EdgeInsets.symmetric(horizontal: 10)),
-                      tabs: [
-                        Tab(
-                          text: '스토리',
-                        ),
-                        Tab(
-                          text: '온라인전시회',
-                        ),
-                        Tab(
-                          text: '아티스트',
-                        ),
-                        Tab(
-                          text: '전시정보',
-                        ),
-                      ],
-                    ),
-                  ),
-                )),
-              )
-            ];
-          },
-          body: TabBarView(children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 18),
-              child: RefreshIndicator(
-                onRefresh: viewModel.refreshList,
-                child: ListView(
-                  physics: const NeverScrollableScrollPhysics(),
-                  children: [
-                    if (viewModel.postsListState.postList.isNotEmpty) ...{
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(0, 20, 0, 20),
-                        child: Text(
-                          '전체 ' +
-                              viewModel.postsListState.postList.length
-                                  .toString(),
-                          style: const TextStyle(fontSize: 15),
+                            const SizedBox(
+                              width: 9,
+                            ),
+                            Icon(
+                              LineIcons.bookmarkAlt,
+                              size: 34,
+                              color: Colors.grey.shade600,
+                            ),
+                            const SizedBox(
+                              width: 4,
+                            ),
+                            Icon(
+                              LineIcons.shoppingCart,
+                              size: 39,
+                              color: Colors.grey.shade600,
+                            ),
+                          ],
                         ),
                       ),
-                      ...viewModel.postsListState.postList
-                          .map((e) => CardViewItem(
-                                model: e,
-                                onTap: () async {
-                                  bool? result = await Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                      builder: (context) => PostPage(id: e.id!),
-                                    ),
-                                  );
+                    ),
+                  ),
+                  SliverAppBar(
+                    automaticallyImplyLeading: false,
+                    pinned: true,
+                    floating: false,
+                    //expandedHeight: 0,
+                    toolbarHeight: 45,
+                    backgroundColor: Colors.white,
+                    flexibleSpace: FlexibleSpaceBar(
+                        background: Padding(
+                      padding: const EdgeInsets.only(left: 2),
+                      child: Container(
+                        decoration: const BoxDecoration(
+                            border: Border(
+                                bottom: BorderSide(
+                                    color: Colors.grey, width: 0.3))),
+                        child: const TabBar(
+                          isScrollable: true,
+                          unselectedLabelColor: Colors.black,
+                          labelColor: Color(0xff405376),
+                          labelStyle: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 15),
+                          indicator: UnderlineTabIndicator(
+                              borderSide: BorderSide(
+                                  width: 2, color: Color(0xff405376)),
+                              insets: EdgeInsets.symmetric(horizontal: 10)),
+                          tabs: [
+                            Tab(
+                              text: '스토리',
+                            ),
+                            Tab(
+                              text: '온라인전시회',
+                            ),
+                            Tab(
+                              text: '아티스트',
+                            ),
+                            Tab(
+                              text: '전시정보',
+                            ),
+                          ],
+                        ),
+                      ),
+                    )),
+                  )
+                ];
+              },
+              body: TabBarView(children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 18),
+                  child: RefreshIndicator(
+                    onRefresh: viewModel.refreshList,
+                    child: ListView(
+                      physics: const NeverScrollableScrollPhysics(),
+                      children: [
+                        if (viewModel.postsListState.postList.isNotEmpty) ...{
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(0, 20, 0, 20),
+                            child: Text(
+                              '전체 ' +
+                                  viewModel.postsListState.postList.length
+                                      .toString(),
+                              style: const TextStyle(fontSize: 15),
+                            ),
+                          ),
+                          ...viewModel.postsListState.postList
+                              .map((e) => CardViewItem(
+                                    model: e,
+                                    onTap: () async {
+                                      bool? result = await Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                          builder: (context) =>
+                                              PostPage(id: e.id!),
+                                        ),
+                                      );
 
-                                  if (result != null && result == true) {
-                                    viewModel.refreshList();
-                                  }
-                                },
-                              ))
-                          .toList()
-                    } else ...{
-                      const SkeletonPostList()
-                    }
-                  ],
+                                      if (result != null && result == true) {
+                                        viewModel.refreshList();
+                                      }
+                                    },
+                                  ))
+                              .toList()
+                        } else ...{
+                          const SkeletonPostList()
+                        }
+                      ],
+                    ),
+                  ),
                 ),
-              ),
+                const Text('다다'),
+                const Text('다다'),
+                const Text('다다'),
+              ]),
             ),
-            const Text('다다'),
-            const Text('다다'),
-            const Text('다다'),
-          ]),
-        ),
-      ),
-    ));
+          ),
+        ));
   }
 }
