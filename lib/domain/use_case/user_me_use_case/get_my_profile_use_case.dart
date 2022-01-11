e636c216b4c639d01903f49dfd55ddc7d279ac51
genie0806 <genie0806@gmail.com>
2022-01-11 17:39:27 +0900

    getMyProfileUseCase 작성

---
+++ lib/domain/use_case/user_me_use_case/get_my_profile_use_case.dart
@@ -0,0 +1,33 @@
+import 'package:virtue_test/core/result.dart';
+import 'package:virtue_test/domain/model/user_me_model/user_me_model.dart';
+import 'package:virtue_test/domain/repository/login_user_repository.dart';
+import 'package:virtue_test/domain/repository/user_me_repository.dart';
+
+class GetMyProfileUseCase {
+  LoginUserRepository loginUserRepository;
+  UserMeRepository userMeRepository;
+
+  GetMyProfileUseCase(
+    this.loginUserRepository,
+    this.userMeRepository,
+  );
+
+  Future<Result<UserMeModel>> call(String username, String password) async {
+    try {
+      final loginResponseModel =
+          await loginUserRepository.getLoginUser(username, password);
+
+      return loginResponseModel.when(success: (loginResponseModel) async {
+        final token = loginResponseModel.data?.token;
+        if (token == null) {
+          return const Result.error('로그인 토큰이 생성되지않았습니다.');
+        }
+        return await userMeRepository.getUserMe(token);
+      }, error: (message) {
+        return const Result.error('로그인 토큰이 생성되지않았습니다.');
+      });
+    } catch (e) {
+      return const Result.error('네트워크가 연결되어있지 않습니다');
+    }
+  }
+}
